d712e0738af5e471cece249cf9617f2f3f620355
Paul Parisot <paul.parisot@epitech.eu>
2023-05-29 16:31:16 +0200

    Feat: Exo4
    - Starts the malloc chain struct

--- includes/malloc.h
+++ includes/malloc.h
@@ -8,4 +8,18 @@
 #ifndef MALLOC_H_
     #define MALLOC_H_
 
+    #include <stddef.h>
+
+typedef struct s_node node_t;
+
+struct s_node {
+    size_t size;
+    void *ptr;
+    struct s_node *next;
+};
+
+typedef struct s_heap {
+    node_t *beggin;
+} heap_t;
+
 #endif /* !MALLOC_H_ */
